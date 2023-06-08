27299b85a556221a2d2a9d66a85f15f0c1a9b15e
Dhruv1238 <dhruv4075@gmail.com>
2023-06-09 01:54:09 +0530

    Cleaning the Code

--- FB-graphapi/src/components/PageConversations/PageConversations.jsx
+++ FB-graphapi/src/components/PageConversations/PageConversations.jsx
@@ -36,12 +36,7 @@ const PageConversations = ({ pageId, accessToken }) => {
                             recipientID: messageDetails.from.id,
                           });
                           console.log(messageDetails.message)
-                          // const messageDetailer = {
-                          //   id: messageDetails.id,
-                          //   message: messageDetails.message,
-                          //   from: messageDetails.from.name,
-                          //   fromId: messageDetails.from.id,
-                          // };
+
                         } else {
                           resolve(null);
                         }
@@ -117,42 +112,6 @@ const PageConversations = ({ pageId, accessToken }) => {
   useEffect(() => {
     console.log("Conversations: ",conversations)
   }, [conversations]);
-// return (
-//   <div className="page-conversations">
-//     {loading && <Loader />}
-//     <ul className="conversation-list">
-//     <h1 className="conversation-heading">Conversations</h1>
-//       {conversations.map((conversation) => (
-//         <li key={conversation.id} className="conversation-item">
-//           <h1 className="conversation-heading">Your Conversation with : </h1>
-//           <ul className="message-list">
-//             {conversation.messages.map((message, index) => (
-//               <li key={index} className="message-item">
-//                 <h3 className="conversation-participant">From:{conversation.messages[index].from}</h3>
-//                 {message.message}
-//               </li>
-//             ))}
-//           </ul>
-//           <div className="reply-section">
-//             <input
-//               type="text"
-//               className="reply-input"
-//               // value={replyText}
-//               // onChange={handleReplyChange}
-//               placeholder="Type your reply..."
-//             />
-//             <button
-//               className="reply-button"
-//               // onClick={() => handleReplySubmit(conversation.id)}
-//             >
-//               Reply
-//             </button>
-//           </div>
-//         </li>
-//       ))}
-//     </ul>
-//   </div>
-// );
 
 return (
   <div className="page-conversations">
@@ -191,79 +150,3 @@ return (
 }
 
 export default PageConversations;
-
-
-
-
-//   const handleReplySubmit = (conversationId) => {
-//     if (replyText) {
-//       const messageData = {
-//         text: replyText
-//       };
-  
-//       window.FB.api(`/${conversationId}/messages`, 'POST', { message: messageData, access_token: accessToken, messaging_type: 'RESPONSE' }, response => {
-//         if (response && response.id) {
-//           // Reply sent successfully, update the conversations
-//           setReplyText('');
-//           getPageConversations();
-//         } else {
-//           console.log(response);
-//           console.log('Failed to send reply.');
-//         }
-//       });
-//     }
-//   };  
-
-
-  // const handleReplyChange = (e) => {
-  //   setReplyText(e.target.value);
-  // };
-
-// const handleReplySubmit = (conversationId) => {
-//   setLoading(true);
-//   if (replyText) {
-//     // Get the conversation details including participants
-//     window.FB.api(`/${conversationId}`, 'GET', { fields: 'participants', access_token: accessToken }, response => {
-//       if (response && response.participants && response.participants.data && response.participants.data.length > 0) {
-//         const participants = response.participants.data;
-//         // Find the recipient ID by excluding the current user's ID
-//         const recipient = participants.find(participant => participant.id !== pageId);
-//         if (recipient) {
-//           const recipientId = recipient.id;
-
-//           const requestData = {
-//             recipient: {
-//               id: recipientId
-//             },
-//             messaging_type: 'RESPONSE',
-//             message: {
-//               text: replyText
-//             }
-//           };
-
-//           window.FB.api(`/${pageId}/messages`, 'POST', { access_token: accessToken }, requestData, response => {
-//             if (response && response.id) {
-//               // Reply sent successfully, update the conversations
-//               setReplyText('');
-//               getPageConversations();
-//               setLoading(false);
-//               toast.success('Reply sent successfully!!');
-//             } else {
-//               console.log(response);
-//               setLoading(false);
-//               toast.error('Failed to send reply.');
-//             }
-//           });
-//         } else {
-//           console.log('Recipient not found in conversation participants.');
-//           setLoading(false);
-//           toast.error('Failed to send reply.');
-//         }
-//       } else {
-//         console.log('No conversation participants found.');
-//         setLoading(false);
-//         toast.error('Failed to send reply.');
-//       }
-//     });
-//   }
-// };
