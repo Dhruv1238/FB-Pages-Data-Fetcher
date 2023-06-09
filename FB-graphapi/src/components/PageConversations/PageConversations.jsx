9b9d323af88a01b923c7f207989bf40330de6b4f
AaryaTiwari1211 <aarya.tiwari@somaiya.edu>
2023-06-09 13:22:21 +0530

    Added Dropdown Menu for Conversations

--- FB-graphapi/src/components/PageConversations/PageConversations.jsx
+++ FB-graphapi/src/components/PageConversations/PageConversations.jsx
@@ -1,156 +1,3 @@
-// import React, { useEffect, useState } from 'react';
-// import Loader from '../Loader/Loader'
-// import './PageConversations.css'
-
-// import { ToastContainer, toast } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
-
-// const PageConversations = ({ pageId, accessToken }) => {
-//   const [conversations, setConversations] = useState([]);
-//   const [replyText, setReplyText] = useState('');
-//   const [loading,setLoading] = useState(false);
-
-//   useEffect(() => {
-//     const getPageConversations = () => {
-//       setLoading(true)
-//       window.FB.api(`/${pageId}/conversations`, 'GET', { access_token: accessToken }, response => {
-//         if (response.data && response.data.length > 0) {
-//           const conversationData = response.data;
-
-//           const getPageMessages = (conversation) => {
-//             return new Promise(resolve => {
-//               window.FB.api(`/${conversation.id}`, 'GET', { fields: 'messages', access_token: accessToken }, messageResponse => {
-//                 console.log("Message response")
-//                 if (messageResponse.messages && messageResponse.messages.data && messageResponse.messages.data.length > 0) {
-//                   const messageData = messageResponse.messages.data;
-
-//                   const getMessage = (message) => {
-//                     return new Promise(resolve => {
-//                       window.FB.api(`/${message.id}`, 'GET', { fields: 'from,id,message,created_time', access_token: accessToken }, messageDetails => {
-//                         if (messageDetails && messageDetails.message) {
-      
-//                           resolve({
-//                             id: messageDetails.id,
-//                             message: messageDetails.message,
-//                             from: messageDetails.from.name,
-//                             recipientID: messageDetails.from.id,
-//                           });
-//                           console.log(messageDetails.message)
-
-//                         } else {
-//                           resolve(null);
-//                         }
-//                       });
-//                     });
-//                   };
-
-//                   Promise.all(messageData.map(message => getMessage(message)))
-//                     .then(messages => {
-//                       resolve({
-//                         id: conversation.id,
-//                         messages: messages.filter(message => message !== null),
-//                       });
-//                     });
-//                 } else {
-//                   resolve(null);
-//                 }
-//               });
-//             });
-//           };
-
-//           Promise.all(conversationData.map(conversation => getPageMessages(conversation)))
-//             .then(conversations => {
-//               setConversations(conversations.filter(conversation => conversation !== null));
-//               console.log("Last Message ", conversations[0].messages[conversations[0].messages.length - 1].recipientID)
-//             });
-//             setLoading(false)
-//             toast.success("Conversations fetched!!")
-//         } else {
-//           setLoading(false)
-//           toast.error('No conversations found.');
-//         }
-//       });
-//     };
-
-//     getPageConversations();
-//   }, [pageId, accessToken]);
-  
-//   const handleReplyChange = (event) => {
-//     setReplyText(event.target.value);
-//   };
-
-//   const handleReplySubmit = (conversationId) => {
-//     if (replyText.trim() !== '') {
-//       const messageData = {
-//         recipient: {
-//           id: conversationId,
-//         },
-//         messaging_type: 'RESPONSE',
-//         message: {
-//           text: replyText,
-//         },
-//       };
-//       setLoading(true)
-//       window.FB.api(`/${pageId}/messages`, 'POST', { access_token: accessToken, ...messageData }, (response) => {
-//         if (response && !response.error) {
-//           setLoading(false)
-//           toast.success('Reply sent successfully!');
-//           setReplyText('');
-
-//         } else {
-//           setLoading(false)
-//           toast.error('Error sending reply.');
-//         }
-//       });
-//     } else {
-//       console.log(response.error)
-//       toast.error('Reply text is empty.');
-//     }
-//   };
-  
-
-//   useEffect(() => {
-//     console.log("Conversations: ",conversations)
-//   }, [conversations]);
-
-// return (
-//   <div className="page-conversations">
-//     {loading && <Loader />}
-//     <ToastContainer />
-//     <ul className="conversation-list">
-//       <h1 className="conversation-heading">Conversations</h1>
-//       {conversations.map((conversation) => (
-//         <li key={conversation.id} className="conversation-item">
-//           <h1 className="conversation-heading">Your Conversation with : </h1>
-//           <ul className="message-list">
-//             {conversation.messages.map((message, index) => (
-//               <li key={index} className="message-item">
-//                 <h3 className="conversation-participant">From:{conversation.messages[index].from}</h3>
-//                 {message.message}
-//               </li>
-//             ))}
-//           </ul>
-//           <div className="reply-section">
-//             <input
-//               type="text"
-//               className="reply-input"
-//               value={replyText}
-//               onChange={handleReplyChange}
-//               placeholder="Type your reply..."
-//             />
-//             <button className="reply-button" onClick={() => handleReplySubmit(conversation.messages[conversation.messages.length-1].recipientID)}>
-//               Reply
-//             </button>
-//           </div>
-//         </li>
-//       ))}
-//     </ul>
-//   </div>
-// );
-// }
-
-// export default PageConversations;
-
 import React, { useEffect, useState } from 'react';
 import Loader from '../Loader/Loader';
 import './PageConversations.css';
@@ -160,26 +7,28 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const PageConversations = ({ pageId, accessToken }) => {
   const [conversations, setConversations] = useState([]);
+  const [selectedConversation, setSelectedConversation] = useState(null);
   const [replyText, setReplyText] = useState('');
   const [loading, setLoading] = useState(false);
-  const [visibleConversations, setVisibleConversations] = useState(5);
+  const [showAll, setShowAll] = useState(false)
 
   useEffect(() => {
     const getPageConversations = () => {
       setLoading(true);
-      window.FB.api(`/${pageId}/conversations`, 'GET', { access_token: accessToken }, (response) => {
+      window.FB.api(`/${pageId}/conversations`, 'GET', { access_token: accessToken }, response => {
         if (response.data && response.data.length > 0) {
           const conversationData = response.data;
 
-          const getPageMessages = (conversation) => {
-            return new Promise((resolve) => {
-              window.FB.api(`/${conversation.id}`, 'GET', { fields: 'messages', access_token: accessToken }, (messageResponse) => {
+          const getPageMessages = conversation => {
+            return new Promise(resolve => {
+              window.FB.api(`/${conversation.id}`, 'GET', { fields: 'messages', access_token: accessToken }, messageResponse => {
+                console.log("Message response");
                 if (messageResponse.messages && messageResponse.messages.data && messageResponse.messages.data.length > 0) {
                   const messageData = messageResponse.messages.data;
 
-                  const getMessage = (message) => {
-                    return new Promise((resolve) => {
-                      window.FB.api(`/${message.id}`, 'GET', { fields: 'from,id,message,created_time', access_token: accessToken }, (messageDetails) => {
+                  const getMessage = message => {
+                    return new Promise(resolve => {
+                      window.FB.api(`/${message.id}`, 'GET', { fields: 'from,id,message,created_time', access_token: accessToken }, messageDetails => {
                         if (messageDetails && messageDetails.message) {
                           resolve({
                             id: messageDetails.id,
@@ -187,6 +36,7 @@ const PageConversations = ({ pageId, accessToken }) => {
                             from: messageDetails.from.name,
                             recipientID: messageDetails.from.id,
                           });
+                          console.log(messageDetails.message);
                         } else {
                           resolve(null);
                         }
@@ -194,11 +44,11 @@ const PageConversations = ({ pageId, accessToken }) => {
                     });
                   };
 
-                  Promise.all(messageData.map((message) => getMessage(message)))
-                    .then((messages) => {
+                  Promise.all(messageData.map(message => getMessage(message)))
+                    .then(messages => {
                       resolve({
                         id: conversation.id,
-                        messages: messages.filter((message) => message !== null),
+                        messages: messages.filter(message => message !== null),
                       });
                     });
                 } else {
@@ -208,12 +58,13 @@ const PageConversations = ({ pageId, accessToken }) => {
             });
           };
 
-          Promise.all(conversationData.map((conversation) => getPageMessages(conversation)))
-            .then((conversations) => {
-              setConversations(conversations.filter((conversation) => conversation !== null));
-              toast.success('Conversations fetched!!');
+          Promise.all(conversationData.map(conversation => getPageMessages(conversation)))
+            .then(conversations => {
+              setConversations(conversations.filter(conversation => conversation !== null));
+              console.log("Last Message ", conversations[0].messages[conversations[0].messages.length - 1].recipientID);
             });
           setLoading(false);
+          toast.success("Conversations fetched!!");
         } else {
           setLoading(false);
           toast.error('No conversations found.');
@@ -224,11 +75,11 @@ const PageConversations = ({ pageId, accessToken }) => {
     getPageConversations();
   }, [pageId, accessToken]);
 
-  const handleReplyChange = (event) => {
+  const handleReplyChange = event => {
     setReplyText(event.target.value);
   };
 
-  const handleReplySubmit = (conversationId) => {
+  const handleReplySubmit = conversationId => {
     if (replyText.trim() !== '') {
       const messageData = {
         recipient: {
@@ -245,8 +96,10 @@ const PageConversations = ({ pageId, accessToken }) => {
           setLoading(false);
           toast.success('Reply sent successfully!');
           setReplyText('');
+
         } else {
           setLoading(false);
+          console.log(response.error);
           toast.error('Error sending reply.');
         }
       });
@@ -255,49 +108,84 @@ const PageConversations = ({ pageId, accessToken }) => {
     }
   };
 
-  const handleShowMore = () => {
-    setVisibleConversations((prevCount) => prevCount + 5);
+  const handleConversationSelect = conversationId => {
+    const selectedConv = conversations.find(conv => conv.id === conversationId);
+    setSelectedConversation(selectedConv);
   };
+
+  const checkLength = (conversation) => {
+    if (conversation.messages.length > 5) {
+      return true
+    }
+    return false;
+  }
+
+  useEffect(() => {
+    console.log("Selected Conversation: ", selectedConversation);
+  }, [selectedConversation]);
 
   return (
     <div className="page-conversations">
       {loading && <Loader />}
       <ToastContainer />
-      <ul className="conversation-list">
-        <h1 className="conversation-heading">Conversations</h1>
-        {conversations.map((conversation) => (
-          <li key={conversation.id} className="conversation-item">
-            <h1 className="conversation-heading">Your Conversation with: {conversation.messages[conversation.messages.length - 1].from}</h1>
-            <ul className="message-list">
-              {conversation.messages.slice(-5).map((message, index) => (
+      <div className="conversation-dropdown">
+        <h1 className="conversation-heading">Select Conversation:</h1>
+        <select className="conversation-select" onChange={event => handleConversationSelect(event.target.value)}>
+          <option value="">Select</option>
+          {conversations.map(conversation => (
+            <option key={conversation.id} value={conversation.id}>{conversation.messages[conversation.messages.length - 1].from}</option>
+          ))}
+        </select>
+      </div>
+      {selectedConversation && (
+        <>
+          <h1 className="conversation-heading">Your Conversation with: {selectedConversation.messages[selectedConversation.messages.length - 1].from}</h1>
+          <ul className="message-list">
+            {!showAll ? (selectedConversation.messages.slice(0, 5).map((message, index) => (
+              <li key={index} className="message-item">
+                <h3 className="conversation-participant">From: {selectedConversation.messages[index].from}</h3>
+                {message.message}
+              </li>
+            ))) : (
+              selectedConversation.messages.map((message, index) => (
                 <li key={index} className="message-item">
-                  <h3 className="conversation-participant">From: {conversation.messages[index].from}</h3>
+                  <h3 className="conversation-participant">From: {selectedConversation.messages[index].from}</h3>
                   {message.message}
                 </li>
-              ))}
-            </ul>
-            <div className="reply-section">
-              <input
-                type="text"
-                className="reply-input"
-                value={replyText}
-                onChange={handleReplyChange}
-                placeholder="Type your reply..."
-              />
-              <button
-                className="reply-button"
-                onClick={() => handleReplySubmit(conversation.messages[conversation.messages.length - 1].recipientID)}
-              >
-                Reply
+              ))
+            )}
+          </ul>
+          <div className="reply-section">
+            <input
+              type="text"
+              className="reply-input"
+              value={replyText}
+              onChange={handleReplyChange}
+              placeholder="Type your reply..."
+            />
+            <button className="reply-button" onClick={() => handleReplySubmit(selectedConversation.messages[selectedConversation.messages.length - 1].recipientID)}>
+              Reply
+            </button>
+          </div>
+          {checkLength(selectedConversation) && (
+            <>
+              <button className='reply-button' style={{
+                margin:'20px'
+              }} onClick={() => setShowAll(!showAll)}>
+                {!showAll ? (
+                  <>
+                    Show More messages
+                  </>
+                ) : (
+                  <>
+                    Show Less Messages
+                  </>
+                )}
+
               </button>
-            </div>
-          </li>
-        ))}
-      </ul>
-      {conversations.length > visibleConversations && (
-        <button className="next-button" onClick={handleShowMore}>
-          Show More
-        </button>
+            </>
+          )}
+        </>
       )}
     </div>
   );
